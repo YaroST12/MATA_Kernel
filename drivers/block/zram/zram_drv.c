165fd67ff735e4ba63c3dc2bb90a4fae384a3e18
Olav Haugan <ohaugan@codeaurora.org>
2014-10-24 08:52:18 -0700

    staging: zram: Rate limit memory allocation errors
    If an error occurs allocating memory for zram we will not be able to
    fullfill the request to store the page in zram. The swap subsystem still
    continues to try to swap out pages to zram even when this error occurs
    since there is currently no facility to stop the swap subsystem from
    swapping out during such errors. This can cause the system to be
    overflowed with logging errors.
    
    Reduce the amount of logging to prevent the kernel log from being filled
    with these error messages
    
    Change-Id: I54b920337749ece59d9ca78fa8b29345ec7b976b
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -48,6 +48,12 @@ static const char *default_compressor = "lz4";
 #define ALLOC_ERROR_LOG_RATE_MS 1000
 
 
+/*
+ * We don't need to see memory allocation errors more than once every 1
+ * second to know that a problem is occurring.
+ */
+#define ALLOC_ERROR_LOG_RATE_MS 1000
+
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
 
@@ -770,8 +776,10 @@ compress_again:
 		if (handle)
 			goto compress_again;
 
-		pr_err("Error allocating memory for compressed page: %u, size=%u\n",
-			index, clen);
+		if (printk_timed_ratelimit(&zram_rs_time,
+					   ALLOC_ERROR_LOG_RATE_MS))
+			pr_err("Error allocating memory for compressed page: %u, size=%u\n",
+			       index, clen);
 		ret = -ENOMEM;
 		goto out;
 	}
