e5cd15a08ffe6c3f16fca3f6fdf6d82181db47c4
Vevek Venkatesan <vevekv@codeaurora.org>
2018-02-19 16:45:50 +0530

    BACKPORT: input: misc: hbtp_input: fix list corruption in completion queue
    Fix list corruption in task completion wait queue, in hbtp_input
    driver.
    
    Patch Diff Between 2202510/1 and 2202510/3
    
    Conflicts:
    	drivers/input/misc/hbtp_input.c
    
    Signed-off-by: Vevek Venkatesan <vevekv@codeaurora.org>
    Signed-off-by: Satyanarayana Dash <sadash@codeaurora.org>
    Change-Id: I5cb4171cfdc368baf6b09a145112afa7b45e9ccb
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/input/misc/hbtp_input.c
+++ drivers/input/misc/hbtp_input.c
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -91,6 +91,7 @@ struct hbtp_data {
 	u32 power_on_delay;
 	u32 power_off_delay;
 	bool manage_pin_ctrl;
+	bool init_completion_done_once;
 	s16 ROI[MAX_ROI_SIZE];
 	s16 accelBuffer[MAX_ACCEL_SIZE];
 	struct kobject *sysfs_kobject;
@@ -610,7 +611,6 @@ static inline long hbtp_input_ioctl_handler(struct file *file, unsigned int cmd,
 	enum hbtp_afe_power_cmd power_cmd;
 	enum hbtp_afe_signal afe_signal;
 	enum hbtp_afe_power_ctrl afe_power_ctrl;
-	static bool init_completion_not_done = true;
 
 	if (cmd == HBTP_SET_TOUCHDATA) {
 		if (!hbtp || !hbtp->input_dev) {
@@ -729,8 +729,6 @@ static inline long hbtp_input_ioctl_handler(struct file *file, unsigned int cmd,
 			mutex_lock(&hbtp->mutex);
 			if (!hbtp->power_suspended) {
 				complete(&hbtp->power_resume_sig);
-				pr_err("%s: ***VVK*** power_resume_sig completed\n",
-					__func__);
 			} else {
 				pr_err("%s: resume signal in wrong state\n",
 					__func__);
@@ -741,8 +739,6 @@ static inline long hbtp_input_ioctl_handler(struct file *file, unsigned int cmd,
 			mutex_lock(&hbtp->mutex);
 			if (hbtp->power_suspended) {
 				complete(&hbtp->power_suspend_sig);
-				pr_err("%s: ***VVK*** power_suspend_sig completed\n",
-					__func__);
 			} else {
 				pr_err("%s: suspend signal in wrong state\n",
 					__func__);
@@ -802,17 +798,13 @@ static inline long hbtp_input_ioctl_handler(struct file *file, unsigned int cmd,
 				return -EFAULT;
 			}
 			mutex_lock(&hbtp->mutex);
-			if (init_completion_not_done) {
-				init_completion(&hbtp->power_resume_sig);
-				init_completion(&hbtp->power_suspend_sig);
-				init_completion_not_done = false;
-				pr_err("%s: ***VVK*** init_completion\n",
-					__func__);
-			} else {
+			if (hbtp->init_completion_done_once) {
 				reinit_completion(&hbtp->power_resume_sig);
 				reinit_completion(&hbtp->power_suspend_sig);
-				pr_err("%s: ***VVK*** reinit_completion\n",
-					__func__);
+			} else {
+				init_completion(&hbtp->power_resume_sig);
+				init_completion(&hbtp->power_suspend_sig);
+				hbtp->init_completion_done_once = true;
 			}
 			hbtp->power_sig_enabled = true;
 			mutex_unlock(&hbtp->mutex);
@@ -1226,8 +1218,6 @@ static int hbtp_fb_suspend(struct hbtp_data *ts)
 		if (ts->power_sig_enabled) {
 			pr_debug("%s: power_sig is enabled, wait for signal\n",
 				__func__);
-			pr_err("%s: *VVK* wait_for_completion power_suspend_sig\n",
-				__func__);
 			mutex_unlock(&hbtp->mutex);
 			rc = wait_for_completion_interruptible_timeout(
 				&hbtp->power_suspend_sig,
@@ -1291,8 +1281,6 @@ static int hbtp_fb_early_resume(struct hbtp_data *ts)
 			if (ts->power_sig_enabled) {
 				pr_err("%s: power_sig is enabled, wait for signal\n",
 					__func__);
-				pr_err("%s: *VVK* wait_for_completion power_resume_sig\n",
-					__func__);
 				mutex_unlock(&hbtp->mutex);
 				rc = wait_for_completion_interruptible_timeout(
 					&hbtp->power_resume_sig,
@@ -1302,8 +1290,6 @@ static int hbtp_fb_early_resume(struct hbtp_data *ts)
 						__func__);
 				}
 				mutex_lock(&hbtp->mutex);
-				pr_err("%s: *VVK* wait_for_completion power_resume done\n",
-					__func__);
 				pr_debug("%s: wait is done\n", __func__);
 			} else {
 				pr_debug("%s: power_sig is NOT enabled\n",
@@ -1502,6 +1488,7 @@ static int __init hbtp_init(void)
 
 	mutex_init(&hbtp->mutex);
 	mutex_init(&hbtp->sensormutex);
+	hbtp->init_completion_done_once = false;
 
 	error = misc_register(&hbtp_input_misc);
 	if (error) {
