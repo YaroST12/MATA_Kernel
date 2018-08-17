a325116c0d2986d0791fda7e52ddd687840c5e35
Yaroslav Furman <yaro330@gmail.com>
2018-08-17 10:10:38 +0300

    Revert "devfreq_boost: avoid boosting devfreq unnecessarily"
    This reverts commit d630aae801a2046902c01137927d68be9db932ff.

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -16,7 +16,6 @@
 #include <linux/devfreq_boost.h>
 #include <linux/fb.h>
 #include <linux/input.h>
-#include "../../kernel/sched/sched.h"
 
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
@@ -42,15 +41,11 @@ static void __devfreq_boost_kick(struct boost_dev *b)
 void devfreq_boost_kick(enum df_device device)
 {
 	struct df_boost_drv *d = df_boost_drv_g;
-	bool boost_needed = false;
 
 	if (!d)
 		return;
 
-	boost_needed = tasks_on_big_cores();
-
-	if (boost_needed)
-		__devfreq_boost_kick(d->devices + device);
+	__devfreq_boost_kick(d->devices + device);
 }
 
 static void __devfreq_boost_kick_max(struct boost_dev *b,
