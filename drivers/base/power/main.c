14c9c2540fbc34d8a503cc80e911f33a3109e63c
Yaroslav Furman <yaro330@gmail.com>
2018-09-05 10:45:20 +0300

    power: main: fix -Wformat warning
    Ew, should be fine.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -129,7 +129,7 @@ void device_pm_add(struct device *dev)
 	mutex_lock(&dpm_list_mtx);
 	if (dev->parent && dev->parent->power.is_prepared)
 		pr_debug("%s: parent %s should not be sleeping\n",
-			dev, dev_name(dev->parent));
+			(char *)dev, dev_name(dev->parent));
 	list_add_tail(&dev->power.entry, &dpm_list);
 	mutex_unlock(&dpm_list_mtx);
 }
