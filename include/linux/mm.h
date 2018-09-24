0111e4570ebf9026dd00c086d3ca1cf3be371b87
Yaroslav Furman <yaro330@gmail.com>
2018-09-24 15:07:59 +0300

    treewide: revert mainline overflow and allocation commits
    This reverts commit 8b85d1c1b56e59c618e250c1a66c8560b7c15f13.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -21,7 +21,6 @@
 #include <linux/resource.h>
 #include <linux/page_ext.h>
 #include <linux/err.h>
-#include <linux/overflow.h>
 
 struct mempolicy;
 struct anon_vma;
@@ -426,21 +425,6 @@ static inline void *kvzalloc_node(size_t size, gfp_t flags, int node)
 static inline void *kvzalloc(size_t size, gfp_t flags)
 {
 	return kvmalloc(size, flags | __GFP_ZERO);
-}
-
-static inline void *kvmalloc_array(size_t n, size_t size, gfp_t flags)
-{
-	size_t bytes;
-
-	if (unlikely(check_mul_overflow(n, size, &bytes)))
-		return NULL;
-
-	return kvmalloc(bytes, flags);
-}
-
-static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
-{
-	return kvmalloc_array(n, size, flags | __GFP_ZERO);
 }
 
 extern void kvfree(const void *addr);
