8c3f46d88f708c658e3589167b2940dbc83e4958
Yaroslav Furman <yaro330@gmail.com>
2018-09-26 09:47:35 +0300

    camera_v2: update video hack for new userdebug hack
    Should work more reliably now.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -215,7 +215,7 @@ void msm_isp_get_timestamp(struct msm_isp_timestamp *time_stamp,
 		time_stamp->buf_time.tv_sec    = time_stamp->vt_time.tv_sec;
 		time_stamp->buf_time.tv_usec   = time_stamp->vt_time.tv_usec;
 	} else {
-		if (userdebug)
+		if (is_userdebug())
 			get_monotonic_boottime(&ts);
 		else
 			ktime_get_ts(&ts);
